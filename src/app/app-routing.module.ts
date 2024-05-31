5cd03425a56ce6877709b652d70a391bc88c05cb
imohammadali <ali1995ez@gmail.com>
2024-05-31 12:29:58 +0330

    chore: add preload strategy for routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,7 +29,9 @@ export const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {
